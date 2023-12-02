4605df1d5b259f6b650204fc6604aaffaa3e6b09
CHINTAMANI PALA <chintamanipala67@gmail.com>
2023-12-02 13:10:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const wrapper = document.querySelector(".wrapper"),
+/*const wrapper = document.querySelector(".wrapper"),
 searchInput = wrapper.querySelector("input"),
 volume = wrapper.querySelector(".word i"),
 infoText = wrapper.querySelector(".info-text"),
@@ -58,5 +58,53 @@ searchInput.addEventListener("keyup", e =>{
         var res=fetchApi(query);
         
     }
+});
+*/
+const wrapper = document.querySelector(".wrapper"),
+  searchInput = wrapper.querySelector("input"),
+  // ... (other variables)
+
+// This function handles the POST request to the new API endpoint
+async function fetchNewApi(query) {
+  const url = 'https://robomatic-ai.p.rapidapi.com/api';
+  const options = {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      'X-RapidAPI-Key': '51a90343e7mshd67878af7f4afaap133dc9jsncea325bfab2f',
+      'X-RapidAPI-Host': 'robomatic-ai.p.rapidapi.com'
+    },
+    body: new URLSearchParams({
+      in: query, // Use the query parameter here
+      op: 'in',
+      cbot: '1',
+      SessionID: 'RapidAPI1',
+      cbid: '1',
+      key: 'RHMN5hnQ4wTYZBGCF3dfxzypt68rVP',
+      ChatSource: 'RapidAPI',
+      duration: '1'
+    })
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.text();
+    console.log(result);
+    // You can handle the API response here as needed
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Event listener for user input
+searchInput.addEventListener("keyup", e => {
+  let word = e.target.value.replace(/\s+/g, ' ');
+  searchInput.addEventListener('input', () => {
+    query = searchInput.value;
+    console.log(query);
+  })
+  if (e.key == "Enter" && word) {
+    fetchNewApi(query); // Call the function to send the query to the new API
+  }
 });
 
