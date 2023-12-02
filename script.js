48f196950bf3d752dfbb144adbd917fdbb51d059
CHINTAMANI PALA <chintamanipala67@gmail.com>
2023-12-02 14:05:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*const wrapper = document.querySelector(".wrapper"),
+const wrapper = document.querySelector(".wrapper"),
 searchInput = wrapper.querySelector("input"),
 volume = wrapper.querySelector(".word i"),
 infoText = wrapper.querySelector(".info-text"),
@@ -59,60 +59,3 @@ searchInput.addEventListener("keyup", e =>{
         
     }
 });
-*/
-const wrapper = document.querySelector(".wrapper"),
-searchInput = wrapper.querySelector("input"),
-volume = wrapper.querySelector(".word i"),
-infoText = wrapper.querySelector(".info-text"),
-synonyms = wrapper.querySelector(".synonyms .list"),
-removeIcon = wrapper.querySelector(".search span");
-removeIcon.addEventListener('click',()=>{
-    searchInput.value="";
-})
-let audio;
-let query="";
-
-// This function handles the POST request to the new API endpoint
-const fetchNewApi= async (query) =>{
-  const url = 'https://robomatic-ai.p.rapidapi.com/api';
-  const options = {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/x-www-form-urlencoded',
-      'X-RapidAPI-Key': '51a90343e7mshd67878af7f4afaap133dc9jsncea325bfab2f',
-      'X-RapidAPI-Host': 'robomatic-ai.p.rapidapi.com'
-    },
-    body: new URLSearchParams({
-      in: query, // Use the query parameter here
-      op: 'in',
-      cbot: '1',
-      SessionID: 'RapidAPI1',
-      cbid: '1',
-      key: 'RHMN5hnQ4wTYZBGCF3dfxzypt68rVP',
-      ChatSource: 'RapidAPI',
-      duration: '1'
-    })
-  };
-
-  try {
-    const response = await fetch(url, options);
-    const result = await response.text();
-    console.log(result);
-    // You can handle the API response here as needed
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-// Event listener for user input
-searchInput.addEventListener("keyup", e => {
-  let word = e.target.value.replace(/\s+/g, ' ');
-  searchInput.addEventListener('input', () => {
-    query = searchInput.value;
-    console.log(query);
-  })
-  if (e.key == "Enter" && word) {
-    fetchNewApi(query); // Call the function to send the query to the new API
-  }
-});
-
