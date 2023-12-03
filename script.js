92a9d259d367bd643b0b8834ad2d621d9824b06c
CHINTAMANI PALA <chintamanipala67@gmail.com>
2023-12-03 10:21:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,23 +14,15 @@ let query="";
 
 
 function fetchApi(query){
-    const options = {
-        method: 'GET',
-        headers: {
-            Authorization: 'q9Oh7Lg7J0Hd',
-            'X-RapidAPI-Key': '51a90343e7mshd67878af7f4afaap133dc9jsncea325bfab2f',
-            'X-RapidAPI-Host': 'random-stuff-api.p.rapidapi.com'
-        }
-    };
-
-    fetch(`https://random-stuff-api.p.rapidapi.com/ai/response?message=${query}&user_id=420`, options)
+    query=query.replace(/ /g,"+") 
+    fetch(`https://chatbot-api-chintamanipala.vercel.app/ask?q={query}`)
         .then(response => response.json())
         .then(response => {
            let info= document.getElementById("info");
            console.log(info);
            info.style.display="none";
            let resu=document.getElementById("result");
-            resu.innerHTML=response["message"];
+            resu.innerHTML=response["answer"];
             if(response["message"]==undefined){
                 resu.innerHTML=`The request to the API has timed out. Please try again later, or if the issue persists, please contact the API provider`
                 
